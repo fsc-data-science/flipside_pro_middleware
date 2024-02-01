747b3e16ba35cdab6f6bef1db69394343df8b59f
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2024-02-01 07:35:43 -0500

    gitignore creds + initial setup

---
+++ pro_charliemarketplace/plumber.R
@@ -0,0 +1,22 @@
+library(plumber)
+
+#* @apiTitle Middleware layer to access pro_charliemarketplace custom tables
+#* @apiDescription CRUD controls to schema storage
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg = "") {
+    list(msg = paste0("The message is: '", msg, "'"))
+}
+
+
+
+
+# Programmatically alter your API
+#* @plumber
+function(pr) {
+    pr %>%
+        # Overwrite the default serializer to return unboxed JSON
+        pr_set_serializer(serializer_unboxed_json())
+}
